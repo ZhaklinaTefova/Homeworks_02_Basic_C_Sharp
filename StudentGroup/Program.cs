929aa3b945b32f7036d7ef0572b673926dbbd0f3
Zhaklina Tefova <zhaklina.tefova@outlook.com>
2023-05-12 09:18:26 +0100

    Add project files.

---
+++ StudentGroup/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace StudentGroup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("StudentGroup");
+
+            /*
+            Make a new console application called StudentGroup
+
+            Make 2 arrays called studentsG1 and studentsG2 and fill them with 5 student names (strings).
+            Get a number from the console between 1 and 2 and print the students from that group in the console.
+            
+            Ex: studentsG1 ["Zdravko", "Petko", "Stanko", "Branko", "Trajko"]
+                Test Data:
+                Enter student group: ( there are 1 and 2 )
+                    1
+
+                Expected Output:
+                    The Students in G1 are:
+                        Zdravko
+                        Petko
+                        Stanko
+                        Branko
+                        Trajko
+            */
+
+            string[] studentsG1 = new string[5] { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+            string[] studentsG2 = new string[5] { "Zhaklina", "Ivana", "Jasmina", "Florina", "Sonja" };
+
+            Console.WriteLine("To see group one press 1, to see group two press 2.");
+            string userInput = Console.ReadLine();
+            if (!int.TryParse(userInput, out int input))
+            {
+                Console.WriteLine("Wrong input!");
+                return;
+            }
+            
+
+            if(input == 1)
+            {
+                Console.Write("Student Group 1: ");
+                foreach (string student in studentsG1)
+                {
+                    Console.Write(student + " ");
+                }
+            } 
+            else if(input == 2)
+            {
+                Console.Write("\nStudents Group 2: ");
+                foreach (string student in studentsG2)
+                {
+                    Console.Write(student + " ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+            
+        }
+    }
+}
---
+++ SumOfEven/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace SumOfEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SumOfEven");
+
+            /*
+            Make a console application called SumOfEven. Inside it create an array of 6 integers. 
+            Find and print the sum of the even numbers from the array:
+
+             Test Data:
+                Enter integer no.1:
+                    4
+                Enter integer no.1:
+                    3
+                Enter integer no.1:
+                    7
+                Enter integer no.1:
+                    3
+                Enter integer no.1:
+                    2
+                Enter integer no.1:
+                    8
+                Expected Output:
+                The result is: 14
+            */
+
+            int count = 6;
+            int[] numbers = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"Enter your number {i+1}: ");
+                string input = Console.ReadLine();
+                if(!int.TryParse(input, out int number))
+                {
+                    Console.WriteLine("Wrong input!");
+                    return;
+                }
+                numbers[i] = number;
+            }
+
+            int sum = 0;
+            Console.Write("Your numbers are:");
+            foreach (int i in numbers)
+            {
+                Console.Write(i + " ");
+                sum += i;
+            }
+            
+            Console.WriteLine("\nSum of your numbers is:" + sum);
+        }
+    }
+}
